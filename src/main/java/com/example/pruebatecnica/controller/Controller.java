869f505edd209f68acdcf67ee1c838b7fcdd6955
ROGER <andressartre@gmail.com>
2026-05-22 16:26:09 -0600

    primer endpoint get de prueba hola mundo

---
+++ src/main/java/com/example/pruebatecnica/controller/Controller.java
@@ -0,0 +1,16 @@
+package com.example.pruebatecnica.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Controller {
+	
+	
+	@GetMapping("/hello")
+	public String decirHola() {
+		return "Hola mundo";
+	}
+
+}
