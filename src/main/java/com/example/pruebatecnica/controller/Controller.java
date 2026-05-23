3bffc27260a1c654fda1bef9a843036a3d634ef0
ROGER <andressartre@gmail.com>
2026-05-23 02:19:24 -0600

    se agrega endpoint post y estructura de carpetas model y service

--- src/main/java/com/example/pruebatecnica/controller/Controller.java
+++ src/main/java/com/example/pruebatecnica/controller/Controller.java
@@ -1,16 +1,26 @@
 package com.example.pruebatecnica.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("api/pet")
 public class Controller {
 	
-	
-	@GetMapping("/hello")
+	private static final Logger log = LoggerFactory.getLogger(Controller.class);
+	@GetMapping
 	public String decirHola() {
 		return "Hola mundo";
+	}
+	
+	@PostMapping
+	public String decirHolau() {
+		return "Hola mundou";
 	}
 
 }
---
+++ src/main/java/com/example/pruebatecnica/service/Service.java
@@ -0,0 +1,5 @@
+package com.example.pruebatecnica.service;
+
+public class Service {
+
+}
